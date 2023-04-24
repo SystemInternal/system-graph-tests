16cc6fc600d9981b7c71a7ce8a2ac03b880ec20f
Nico <jnico.chilla@gmail.com>
2023-04-23 21:15:13 -0400

    topological graph working!

--- js/topological.js
+++ js/topological.js
@@ -42,7 +42,7 @@ window.addEventListener('resize', setSize);
 
 function setSize() {
   let sidebar = parseInt(getComputedStyle(document.body).getPropertyValue('--sidebar').replace('px', ''));
-  w = window.innerWidth - sidebar + 20;
+  w = window.innerWidth - sidebar - 20;
   h = window.innerHeight - 40;
   d3.select(graph_svg).attr('width', w + 'px');
   d3.select(graph_svg).attr('height', h + 'px');
@@ -51,7 +51,7 @@ function setSize() {
 }
 
 
-fetch('data/dag-link-data.json')
+fetch('data/topo-data.json')
   .then((response) => response.json())
   .then((data) => {
     nodes = data.nodes;
@@ -69,7 +69,7 @@ function init(link_lists, nodes) {
   for (let key of keys) {
     let parts = key.split('_')
     map[key] = {
-      count: link_lists[key].length
+      count: parts[0] == 'p0' && key == 'p0_downstream' || parts[0] == 'p1' && key == 'p1_upstream' ? 0 : link_lists[key].length
     };
 
     if (parts[0] !== 'primary') {
@@ -77,7 +77,9 @@ function init(link_lists, nodes) {
       let range = map[key].field.querySelector('input');
       let counter = map[key].field.querySelector('.count');
       range.max = link_lists[key].length;
-      range.value = link_lists[key].length;
+      range.value = parts[0] == 'p0' && key == 'p0_downstream' || parts[0] == 'p1' && key == 'p1_upstream' ? 0 : link_lists[key].length;
+
+
       // console.log(link_lists[key].length+']');
       counter.style.setProperty('--max', `"/${link_lists[key].length + ']'}"`);
       counter.innerText = link_lists[key].length;
@@ -137,14 +139,20 @@ function init(link_lists, nodes) {
     row.appendChild(text);
     key_section.appendChild(row);
   }
-  
+
   key_section.querySelector('input[type="checkbox"]').addEventListener('click', function () {
     categories_visible = key_section.querySelector('input[type="checkbox"]').checked;
     if (key_section.querySelector('input[type="checkbox').checked) main.classList.add('see-categories')
     else main.classList.remove('see-categories')
 
+    graph.spacer=categories_visible?24:18;
     graph_input = generate_graph_input();
     graph.update(graph_input.nodes, graph_input.links);
+  })
+
+  let directionality_input=document.querySelector('select[name="directionality"');
+  directionality_input.addEventListener('change',function(){
+    graph_svg.dataset.directionality=directionality_input.value;
   })
 
   document.querySelectorAll('input.primary').forEach((checkbox) => {
@@ -173,6 +181,27 @@ function init(link_lists, nodes) {
 
     })
   })
+
+  let start_pos=[0,0];
+  let drag_pos=[0,0];
+  graph_svg.addEventListener('mousedown',function(){
+    start_pos=[event.clientX - drag_pos[0],event.clientY - drag_pos[1]];
+    graph_svg.classList.add('drag')
+    graph_svg.addEventListener('mousemove',graph_drag)
+  })
+
+  graph_svg.addEventListener('mouseup',function(){
+    graph_svg.classList.remove('drag')
+    // graph_svg.style.setProperty('--drag-x',0)
+    // graph_svg.style.setProperty('--drag-y',0)
+    graph_svg.removeEventListener('mousemove',graph_drag)
+  })
+
+  function graph_drag(){
+    graph_svg.style.setProperty('--drag-x',event.clientX-start_pos[0]+'px')
+    graph_svg.style.setProperty('--drag-y',event.clientY-start_pos[1]+'px')
+    drag_pos=[event.clientX-start_pos[0],event.clientY-start_pos[1]]
+  }
 
 }
 
@@ -231,12 +260,124 @@ function generate_graph_input() {
 const Graph = class {
   constructor(nodes, links, svg) {
 
-    console.log(nodes,links,svg);
+    this.nodes = nodes;
+    this.links = links;
+    this.box = d3.select(svg);
+
+    this.spacer = 18;
+
+    this.x_margin="6";
+    this.y_margin="0";
+
+    this.link = this.box.append("g")
+      .attr('class', 'link-lines')
+      .attr("stroke", "black")
+      .attr("stroke-opacity", 1)
+      .attr("stroke-width", 1)
+      .attr("stroke-linecap", "round")
+      .attr("vector-effect", "non-scaling-stroke")
+      .selectAll("path")
+
+    this.animate_link = this.box.append("g")
+      .attr('class', 'animate')
+      .attr("stroke", "black")
+      .attr("stroke-opacity", 1)
+      .attr("stroke-width", 1)
+      .attr("stroke-linecap", "round")
+      .attr("vector-effect", "non-scaling-stroke")
+      .selectAll("path")
+
+
+    this.arrow = this.box.append('g')
+      .attr('class', 'link-arrows')
+      .attr("stroke-linecap", "round")
+      .attr("vector-effect", "non-scaling-stroke")
+      .selectAll('path')
+
+
+
+    this.node = this.box
+      .append('g')
+      .attr('class', 'node-circles')
+      .attr("fill", "currentColor")
+      .attr("stroke", "#fff")
+      .attr("stroke-opacity", 1)
+      .attr("stroke-width", 1)
+      .selectAll("circle")
+    
+    this.label_bg=this.box
+      .append("g")
+      .attr('class','node-label-bgs')
+      .selectAll("rect")
+
+    this.label = this.box
+      .append('g')
+      .attr('class', 'node-labels')
+      .selectAll('text')
+
+
+    this.update(nodes, links);
+
+
+
+
+    console.log(nodes, links, svg);
+  }
+
+  index_by_group() {
+    this.counts = {
+      feedback: 0,
+      confounder: 0,
+      mediator: 0,
+      upstream_p0: 0,
+      downstream_p1: 0,
+      upstream_p1: 0,
+      downstream_p0: 0
+    }
+
+
+    for (let node of this.nodes) {
+      node.i = this.counts[node.group];
+      this.counts[node.group]++;
+    }
+    console.log(this.spacer)
+    this.heights = {
+      upstream_p0: this.counts.upstream_p0 * this.spacer,
+      downstream_p1: this.counts.downstream_p1 * this.spacer,
+      confounder: this.counts.confounder * this.spacer,
+      mediator: this.counts.mediator * this.spacer,
+      feedback: this.counts.feedback * this.spacer
+    }
+
+    for (let node of this.nodes) {
+      node.x = this.place_x(node);
+      node.y = this.place_y(node);
+    }
+  }
+
+  place_x(node) {
+    if (node.group == 'p0') return w * 0.3;
+    else if (node.group == 'p1') return w * 0.7;
+    else if (node.group == 'upstream_p0' || node.group == 'confounder') return 20;
+    else if (node.group == 'downstream_p1') return w * 0.9;
+    else if (node.group == 'mediator' || node.group == 'feedback') return w * 0.5;
+
+
+  }
+
+  place_y(node) {
+    if (node.group == 'p0' || node.group == 'p1') return h * 0.5;
+    else if (node.group == 'upstream_p0') return h * 0.5 - (this.heights.upstream_p0 * 0.5) + node.i * this.spacer;
+    else if (node.group == 'downstream_p1') return h * 0.5 - this.heights.downstream_p1 * 0.5 + node.i * this.spacer;
+    else if (node.group == 'mediator') return h * 0.7 + node.i * this.spacer;
+    else if (node.group == 'feedback') return h * 0.3 - this.heights.feedback + node.i * this.spacer;
+    else if (node.group == 'confounder') return h * 0.5 + this.heights.upstream_p0 * 0.5 + (node.i + 2) * this.spacer;
   }
 
   update(nodes, links) {
     // Make a shallow copy to protect against mutation, while
     // recycling old nodes to preserve position and velocity.
+
 
     const old_node = new Map(this.node.data().map(d => [d.val, d]));
     const old_link = new Map(this.link.data().map(d => [d.source.val + '-' + d.target.val, d]));
@@ -244,7 +385,203 @@ const Graph = class {
     nodes = nodes.map(d => Object.assign(old_node.get(d.val) || {}, d));
     links = links.map(d => Object.assign(old_link.get(d.source + '-' + d.target) || {}, d));
 
-    console.log(nodes,links);
+    this.nodes = nodes;
+    this.links = links;
+    this.index_by_group(this.nodes);
+
+
+    const link_gen = d3.linkHorizontal();
+    const link_vert=d3.linkVertical();
+
+    let labels = [
+      {
+        group: 'upstream_p0',
+        i: -1
+      },
+      {
+        group: 'downstream_p1',
+        i: -1
+      },
+      {
+        group: 'feedback',
+        i: -1
+      },
+      {
+        group: 'confounder',
+        i: -1
+      },
+      {
+        group: 'mediator',
+        i: -1
+      }
+    ]
+
+    for (let label of labels) {
+      d3.select(`#${label.group}_label`)
+        .attr('x', this.place_x(label))
+        .attr('y', this.place_y(label))
+    }
+
+    this.animate_link = this.animate_link
+      .data(links, d => d.source + '-' + d.target)
+      .join(
+        enter => enter.append('path')
+          .attr('class', (d) => d.type)
+          .attr('d', d => {
+            let src = this.nodes.find(a => a.val == d.source);
+            let trg = this.nodes.find(a => a.val == d.target);
+            return link_gen({
+              source: [src.x, src.y],
+              target: [trg.x, trg.y]
+            })
+          })
+          .each((d,i,nodes)=>{
+            nodes[i].style.animation = 'none';
+            nodes[i].offsetHeight; /* trigger reflow */
+            nodes[i].style.animation = null; 
+          })
+          .style('--str',(d,i,nodes)=>nodes[i].getTotalLength()),
+        update => update
+          .attr('d', d => {
+            let src = this.nodes.find(a => a.val == d.source);
+            let trg = this.nodes.find(a => a.val == d.target);
+            return link_gen({
+              source: [src.x, src.y],
+              target: [trg.x, trg.y]
+            })
+          })
+          .each((d,i,nodes)=>{
+            nodes[i].style.animation = 'none';
+            nodes[i].offsetHeight; /* trigger reflow */
+            nodes[i].style.animation = null; 
+          })
+          .style('--str',(d,i,nodes)=>nodes[i].getTotalLength())
+      )
+
+    this.link = this.link
+      .data(links, d => d.source + '-' + d.target)
+      .join(
+        enter => enter.append('path')
+          .attr('class', (d) => d.type)
+          .attr('id',d=>d.source + '-' + d.target)
+          .attr('d', d => {
+            let src = this.nodes.find(a => a.val == d.source);
+            let trg = this.nodes.find(a => a.val == d.target);
+            return link_gen({
+              source: [src.x, src.y],
+              target: [trg.x, trg.y]
+            })
+          }).each(function (d,i,nodes) {
+            // console.log(d,i,nodes[i])
+            d.point=get_halfway_point(nodes[i])
+          }),
+        update => update
+          .attr('d', d => {
+            let src = this.nodes.find(a => a.val == d.source);
+            let trg = this.nodes.find(a => a.val == d.target);
+            return link_gen({
+              source: [src.x, src.y],
+              target: [trg.x, trg.y]
+            })
+          })
+          .each(function (d,i,nodes) {
+            d.point=get_halfway_point(nodes[i])
+          })
+      )
+
+
+    this.arrow = this.arrow
+      .data(links, d => d.source + '-' + d.target)
+      .join(enter => enter.append('path')
+        .attr('d','M0.950256 1L2.95026 3L4.95026 5L0.950256 9')
+        .attr('id',d=>d.type=='primary'?'arrow-primary':'')
+        .style('transform',d=>`translate(${d.point.x}px,${d.point.y}px) rotate(${d.point.angle + 180}deg) translateX(-2px) translateY(-5px)`),
+        update=>update
+        .style('transform',d=>`translate(${d.point.x }px,${d.point.y}px) rotate(${d.point.angle + 180}deg) translateX(-2px) translateY(-5px)`)
+        )
+  
+
+    this.node = this.node
+      .data(nodes, (d) => d.val)
+      .join(enter => enter.append('circle')
+        .attr("r", 3)
+        .attr('data-val', (d) => d.val)
+        .attr('data-group', (d) => d.group)
+        .attr('class', (d) => `node ${d.primary ? 'primary' : ''}`)
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y),
+        update => update
+          .attr("cx", d => d.x)
+          .attr("cy", d => d.y))
+
+    
+          
+    
+
+
+    this.label = this.label
+      .data(nodes, (d) => d.val)
+      .join(
+        enter => enter.append('text')
+          .attr('class', (d) => `noselect ${d.primary ? 'primary' : ''} ${d.type ? 'has-type' : ''}`)
+          .attr('data-val', (d) => d.val)
+          .text((d) => d.val)
+          .each(function (d) { 
+            
+            d.bbox = this.getBBox();
+            console.log(d.bbox) 
+          })
+          .attr("x", d => d.x)
+          .attr("y", d => d.y),
+        update => update
+          .attr("x", d => d.x)
+          .attr("y", d => d.y)
+      )
+    
+    this.label_bg=this.label_bg
+      .data(nodes,(d)=>d.val)
+      .join(enter=>enter.append('rect')
+          .attr('class',(d)=>d.primary?'primary':'')
+          .style('fill',(d)=>d.type?`var(--cat-${d.type})`:'none')
+          .attr('rx',d => d.bbox.height/2)
+          .attr('ry',d => d.bbox.height/2)
+          .attr('data-type',(d)=>d.type)
+          .attr("width", d =>{
+            console.log(d)
+            return d.bbox.width + this.x_margin*2 + 'px'
+          })
+          .attr("height", d => d.bbox.height + this.y_margin*2 + 'px')
+          .attr("x", d => d.x)
+          .attr("y", d => d.y),
+        update=>update
+          .attr("x", d => d.x)
+          .attr("y", d => d.y),
+      )
+
+
+
+    //set node group based on:
+    // p0 or p1
+    // -> loop?
+    // -> confounder?
+    // -> mediator?
+    // -> upstream p0
+    // -> downstream p1
+    // other stuff (pre-filter out)
+
+
+
+
+    function get_halfway_point(path) {
+      let length = path.getTotalLength();
+      let half = path.getPointAtLength(length / 2 + 10);
+      let half_offset = path.getPointAtLength(length / 2 + 10.1);
+      return {
+        x: half.x,
+        y: half.y,
+        angle: Math.atan2(half.y - half_offset.y, half.x - half_offset.x) * (180 / Math.PI)
+      }
+    }
 
 
   }
